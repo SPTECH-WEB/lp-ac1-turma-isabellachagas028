d175875c355cf96fde0689903881da86772a6b23
isabellachagas028 <isabella.chagas@sptech.school>
2025-09-02 12:39:11 -0300

    RA: final 028 -Isabella Vitória de Araujo Chagas

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Boa sorte!");
         System.out.println("Você pode testar manualmente a classe Turma aqui se quiser....");
+        Turma turmaA=new Turma("teste",2.2);
+        Turma turmaB=new Turma("teste1",2.3);
+        turmaB.desativar();
+        turmaA.matricular(10);
+        turmaA.transferir(turmaB,5);
+        System.out.println(turmaA.getQtdAluno());
+        System.out.println(turmaB.getQtdAluno());
     }
 }
--- src/main/java/school/sptech/Turma.java
+++ src/main/java/school/sptech/Turma.java
@@ -2,5 +2,77 @@ package school.sptech;
 
 public class Turma {
 
-    
+
+
+    private String nome;
+    private  Double mensalidade;
+private   Integer qtdAluno;
+private Boolean ativa;
+ public Turma(String nome, Double mensalidade){
+     this.nome=nome;
+     this.mensalidade=mensalidade;
+     qtdAluno=0;
+     ativa=true;
+ }
+ public void matricular(Integer qtd){
+     if(qtd!=null && qtd>0 && ativa==true){
+         qtdAluno+=qtd;
+     }
+ }
+ public  Integer cancelar(Integer qtd){
+     if (qtd!=null && qtd>0 && qtd<=qtdAluno && ativa==true){
+         qtdAluno-=qtd;
+         return qtd;
+     }
+     else{
+     return null;}
+ }
+ public Integer desativar(){
+     if(ativa==true){
+         ativa=false;
+        Integer qtdAntiga=qtdAluno;
+         qtdAluno=0;
+         return qtdAntiga;
+
+     }
+     else {
+         return null;
+     }
+ }
+
+
+ public Boolean reajustarMensalidade(Double reajuste){
+     if(reajuste!=null && reajuste>0){
+         mensalidade=(mensalidade*(1+reajuste));
+         return true;
+     }
+     else{
+         return false;
+     }
+ }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getMensalidade() {
+        return mensalidade;
+    }
+
+    public Integer getQtdAluno() {
+        return qtdAluno;
+    }
+
+    public Boolean getAtiva() {
+        return ativa;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void transferir(Turma destino, Integer qtdATransferir){
+     if(qtdATransferir!=null &&  qtdATransferir>0 && ativa==true && destino.ativa==true && qtdAluno<=qtdATransferir){
+         qtdAluno=qtdAluno- qtdATransferir;
+         destino.qtdAluno= destino.qtdAluno-qtdATransferir;
+     }
+    }
 }
